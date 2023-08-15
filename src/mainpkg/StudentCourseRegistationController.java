c1ec62b759935f138981a70ebc4adb82e44f19db
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2023-08-15 20:36:31 +0600

    Faculty FXML creating

--- src/mainpkg/StudentCourseRegistationController.java
+++ src/mainpkg/StudentCourseRegistationController.java
@@ -4,9 +4,18 @@
  */
 package mainpkg;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -15,6 +24,9 @@ import javafx.fxml.Initializable;
  */
 public class StudentCourseRegistationController implements Initializable {
 
+    @FXML
+    private Label course;
+
     /**
      * Initializes the controller class.
      */
@@ -22,5 +34,14 @@ public class StudentCourseRegistationController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void backStudentDeshboardOnClick(ActionEvent event) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("StudentDeshBoard.fxml"));
+        Scene newScene = new Scene (parent);
+        Stage newStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        newStage.setScene(newScene);
+        newStage.show();
+    }
     
 }
